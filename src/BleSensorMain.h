473608ec2cdc9ecb7ab5eda43dda4551daf4c1e7
Robert Thomitzek <thro@thomitzek.com>
2023-09-28 21:08:52 +0200

    Start

---
+++ src/BleSensorMain.h
@@ -0,0 +1,9 @@
+
+#ifdef USE_TH1
+#include "ibs-th1.h"
+#define IBS_TYPE ibs_th1
+#endif
+#ifdef USE_TH2
+#include "ibs-th2.h"
+#define IBS_TYPE ibs_th2
+#endif
---
+++ src/config.h
@@ -0,0 +1,9 @@
+
+#define DEVICE_ADDRESS {0x49,0x22,0x07,0x07,0x0b,0x02}
+#define DEVICE_NAME "sps"
+#define SERVICE_UUID "fff0"
+#define CHAR_UUID    "fff2"
+
+/*select type of sensor*/
+#define USE_TH1
+//#define USE_TH2
